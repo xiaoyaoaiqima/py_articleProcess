92f8a7b0c9a8c7160050094e49120c20d611f0de
SamLin <1428293926@qq.com>
2024-04-11 00:16:20 +0800

    1

---
+++ front/src/App.vue
@@ -0,0 +1,27 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+import panel from './components/panel.vue'
+import upload from './components/upload.vue'
+</script>
+
+<template>
+  <div id="upload">
+    <upload ></upload>
+  </div>
+  <div id="panel">
+    <panel></panel>
+  </div>
+</template>
+
+<style scoped>
+#upload {
+  position: absolute;
+  top: 60%;
+  left: 50%;
+}
+#panel {
+  position: absolute;
+  top: 10%;
+  left: 40%;
+}
+</style>
---
+++ front/src/components/panel.vue
@@ -0,0 +1,60 @@
+<template>
+  <a-collapse v-model:activeKey="activeKey" ghost style="width: 50rem">
+    <a-collapse-panel key="1" header="学校" :show-arrow="false" style="display: inline-block;">
+      <div>
+        <a-button v-for="school in schools" :key="school" type="text"
+                  @click="handleButtonClick('1', school)">{{ school }}
+        </a-button>
+      </div>
+    </a-collapse-panel>
+    <a-collapse-panel key="2" header="学院" :show-arrow="false">
+      <div>
+        <a-button v-for="college in BJcolleges" :key="college" type="text"
+                  @click="handleButtonClick('2', college)">{{ college }}
+        </a-button>
+      </div>
+    </a-collapse-panel>
+    <a-collapse-panel key="3" header="专业" collapsible="disabled">
+      <p>{{ text }}</p>
+    </a-collapse-panel>
+    <a-collapse-panel key="4" header="教师" collapsible="disabled">
+      <p>{{ text }}</p>
+    </a-collapse-panel>
+    <a-collapse-panel key="4" header="专业课程" collapsible="disabled">
+      <p>{{ text }}</p>
+    </a-collapse-panel>
+  </a-collapse>
+</template>
+<script setup>
+import {BJcolleges, BJlessons, BJmajors, BJteachers, schools} from '../assets/static.js'
+import {ref, watch} from 'vue';
+
+const activeKey = ref(['1']);
+const results = ref({});
+
+const handleButtonClick = (panelKey, btnKey) => {
+  console.log(panelKey)
+  activeKey.value = [panelKey]; // 设置激活的 panel key
+  // if (panelKey===)
+  results.value[panelKey] = btnKey; // 存储点击结果
+  // console.log('Button clicked:', btnKey); // 处理按钮点击事件
+};
+
+watch(activeKey, val => {
+  // console.log(val);
+});
+
+
+const Clicked_btn = ref({
+  btn1:0,
+  btn2:0,
+  btn3:0,
+  btn4:0,
+  btn5:0
+});
+
+
+watch(results, val => {
+  Clicked_btn.value = val['1']; // 获取学校按钮的点击结果
+});
+</script>
